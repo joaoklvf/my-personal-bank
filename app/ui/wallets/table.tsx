d17139cb9a955ba4e359c0e771265a2761b495ed
João Figueredo <joaoklvf@gmail.com>
2024-08-08 18:53:38 -0300

    feat:  wallet sm table

--- app/ui/wallets/table.tsx
+++ app/ui/wallets/table.tsx
@@ -14,14 +14,32 @@ export default async function WalletsTable({
     <div className="mt-6 flow-root">
       <div className="inline-block min-w-full align-middle">
         <div className="rounded-lg bg-gray-50 p-2 md:pt-0">
+          <div className="md:hidden">
+            {wallets?.map((wallet) => (
+              <div
+                key={wallet.id}
+                className="mb-2 w-full rounded-md bg-white p-4"
+              >
+                <div className="flex w-full items-center justify-between pt-4 gap-3 items-stretch">
+                  <div className="w-full text-center rounded content-center" style={{ backgroundColor: wallet.wallet_background_color, color: wallet.wallet_color }}>
+                    <b>{wallet.wallet_description}</b>
+                  </div>
+                  <div className="flex justify-end gap-2">
+                    <UpdateWallet id={wallet.id} />
+                    <DeleteWallet id={wallet.id} />
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
           <table className="hidden min-w-full text-gray-900 md:table">
             <thead className="rounded-lg text-left text-sm font-normal">
               <tr>
-                <th scope="col" className="px-4 py-5 font-medium sm:pl-6">
+                <th scope="col" className="px-4 py-5 sm:pl-6">
                   Carteira
                 </th>
                 <th scope="col" className="relative py-3 pl-6 pr-3">
-                  <span className="sr-only">Edit</span>
+                  Ações
                 </th>
               </tr>
             </thead>
@@ -32,7 +50,7 @@ export default async function WalletsTable({
                   className="w-full border-b py-3 text-sm last-of-type:border-none [&:first-child>td:first-child]:rounded-tl-lg [&:first-child>td:last-child]:rounded-tr-lg [&:last-child>td:first-child]:rounded-bl-lg [&:last-child>td:last-child]:rounded-br-lg"
                 >
                   <td className="whitespace-nowrap py-3 pl-6 pr-3 w-full">
-                    <div className="flex items-center gap-3 rounded-lg h-10 justify-center" style={{ backgroundColor: wallet.wallet_background_color, color: wallet.wallet_color }}>
+                    <div className="flex items-center rounded-lg justify-center h-9 lh-0.5" style={{ backgroundColor: wallet.wallet_background_color, color: wallet.wallet_color }}>
                       <b>{wallet.wallet_description}</b>
                     </div>
                   </td>
