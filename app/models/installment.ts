1146d4587a16bff6cda9872bc5c9615158e009f3
João Figueredo <joaoklvf@gmail.com>
2024-08-08 12:27:34 -0300

    fix: installment payment status

--- app/models/installment.ts
+++ app/models/installment.ts
@@ -1,15 +1,15 @@
 import { Model } from './model';
-import { Status } from './payment-status';
+import { PaymentStatus } from './payment-status';
 
 export class Installment extends Model {
   public installment_number: number;
   public installment_date: Date;
-  public status: Status;
+  public installment_payment_status: PaymentStatus;
 
-  constructor(id: string, installmentNumber: number, installmentDate: Date, status: Status) {
+  constructor(id: string, installmentNumber: number, installmentDate: Date, status: PaymentStatus) {
     super(id);
     this.installment_number = installmentNumber;
     this.installment_date = installmentDate;
-    this.status = status;
+    this.installment_payment_status = status;
   }
 }
